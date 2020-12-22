a1186f8d73d99113c2346789f288ddda4081ac92
Ross Leitch <professional.balbatross@gmail.com>
2020-12-22 22:50:15 +1300

    Init

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 global.ENVIRONMENT = (typeof process !== 'undefined') && (process.release.name === 'node') ? 'NODE' : 'BROWSER'
 
+const fs = require('fs')
 const { generate } = require('libp2p/src/pnet')
 const IPFS = require('ipfs')
 const P2PStack = require('./lib/p2p-stack')
-
 
 const  WorkhubIPFS = async (config = {}, swarmKey) => {
     if(ENVIRONMENT == "NODE" && !swarmKey) {
         swarmKey = new Uint8Array(95);
         generate(swarmKey)
     }
+
     let node = await IPFS.create({
         repo: config.repo || 'workhub',
         libp2p: P2PStack(swarmKey),
@@ -22,7 +23,28 @@ const  WorkhubIPFS = async (config = {}, swarmKey) => {
         relay: {enabled: true, hop: {enabled: true}}
     })
 
-    return node;
+    const getFile = async (cid, tmpPath) => {
+        let content = Buffer.from('')
+        for await (const chunk of node.cat(cid)){
+          content = Buffer.concat([content, chunk])
+        }
+    
+      fs.writeFileSync(tmpPath, content)
+      console.log("Gotten file", cid)
+    }
+    
+    const addFile = async (file) => {
+      console.log("Adding file")
+      const id = uuidv4()
+      const result = await node.add(file)
+      return result.cid;
+    }
+
+    return {
+        ipfs: node,
+        getFile,
+        addFile
+    }
 }
 
 module.exports = WorkhubIPFS
