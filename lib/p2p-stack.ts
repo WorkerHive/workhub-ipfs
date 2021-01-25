e32305f344349a1b99da9445cd4db24e25925867
Ross Leitch <professional.balbatross@gmail.com>
2021-01-25 17:21:32 +1300

    Triage wrtc issue in browser

--- lib/p2p-stack.ts
+++ lib/p2p-stack.ts
@@ -1,12 +1,16 @@
 const ENVIRONMENT = (typeof process !== 'undefined') && (process.release && process.release.name === 'node') ? 'NODE' : 'BROWSER'
 
 const MDNS = require('libp2p-mdns')
-if(ENVIRONMENT == "NODE") var TCP = require('libp2p-tcp')
+let TCP: any, wrtc : any;
+if(ENVIRONMENT == "NODE") {
+    console.log("ENV", ENVIRONMENT)
+    TCP = require('libp2p-tcp')
+    wrtc = require('wrtc')
+}
 const MPLEX = require('libp2p-mplex');
 const NOISE = require('libp2p-noise').NOISE;
 const Protector = require('libp2p/src/pnet');
 const WebRTCStar = require('libp2p-webrtc-star')
-if(ENVIRONMENT == "NODE") var wrtc = require('wrtc')
 
 const transportKey = WebRTCStar.prototype[Symbol.toStringTag]
 
@@ -20,11 +24,13 @@ const peerDiscovery = {
         enabled: true
     }
 }
-if(ENVIRONMENT == "NODE") peerDiscovery[MDNS.tag] = {
-    enabled: true
-}
 
-if(ENVIRONMENT == "NODE") wrtcTransport.wrtc = wrtc
+if(ENVIRONMENT == "NODE") {
+    peerDiscovery[MDNS.tag] = {
+        enabled: true
+    }
+    wrtcTransport.wrtc = wrtc
+}
 
 export const P2PStack = (swarmKey: Uint8Array) => ({
     modules: {
